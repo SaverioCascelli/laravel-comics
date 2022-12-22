4d16b628c18915298c064be085059b3113ac7032
SaverioCascelli <saverio.cascelli@gmail.com>
2022-12-22 17:32:44 +0100

    [add] "comics" route & blade section

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,35 @@
 <header>
-    <p>header</p>
+
+    <div class="logo">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo dc">
+    </div>
+    <ul>
+        <li>
+            <a href="{{ route('characters') }}">Characters</a>
+        </li>
+        <li>
+            <a href="{{ route('comics') }}">Comics</a>
+        </li>
+        <li>
+            <a href="{{ route('movies') }}">Movies</a>
+        </li>
+        <li>
+            <a href="{{ route('tv') }}">Tv</a>
+        </li>
+        <li>
+            <a href="{{ route('games') }}">Games</a>
+        </li>
+        <li>
+            <a href="{{ route('videos') }}">Videos</a>
+        </li>
+        <li>
+            <a href="{{ route('fans') }}">Fans</a>
+        </li>
+        <li>
+            <a href="{{ route('news') }}">News</a>
+        </li>
+        <li>
+            <a href="{{ route('shop') }}">Shop</a>
+        </li>
+    </ul>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,42 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-})->name('home');
+    return view('characters');
+})->name('characters');
+
+Route::get('/comics', function () {
+    return view('comics');
+})->name('comics');
+
+Route::get('/movies', function () {
+    return view('movies');
+})->name('movies');
+
+Route::get('/tv', function () {
+    return view('tv');
+})->name('tv');
+
+Route::get('/comics', function () {
+    return view('comics');
+})->name('comics');
+
+Route::get('/games', function () {
+    return view('games');
+})->name('games');
+
+
+Route::get('/videos', function () {
+    return view('videos');
+})->name('videos');
+
+Route::get('/fans', function () {
+    return view('fans');
+})->name('fans');
+
+Route::get('/news', function () {
+    return view('news');
+})->name('news');
+
+Route::get('/shop', function () {
+    return view('shop');
+})->name('shop');
